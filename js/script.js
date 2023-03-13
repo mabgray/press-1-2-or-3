5ebaa5f4d53043928a2e59592e67b4f1a830ecb2
mab_gray <m2bbie@gmail.com>
2023-03-13 14:08:16 -0500

    InnerHTML

--- js/script.js
+++ js/script.js
@@ -4,3 +4,16 @@ var box2 = document.querySelector(".box-2");
 var box3 = document.querySelector(".box-3");
 
 
+document.addEventListener("keydown", function(e){
+    var key = e.key;
+    if(key == 1){
+        box1.innerText = "🌲";
+
+    } else if (key == 2){
+        box2.innerText = "🪅";
+    } else if (key == 3){
+        box3.innerHTML = "💃";
+
+    }
+
+});
