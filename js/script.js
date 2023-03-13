3acfb8fcbf3a8c8a020ca4ba820310714a28181c
mab_gray <m2bbie@gmail.com>
2023-03-13 14:29:14 -0500

    Clear button

--- js/script.js
+++ js/script.js
@@ -2,18 +2,28 @@ var box1 = document.querySelector(".box-1");
 var box2 = document.querySelector(".box-2");
 
 var box3 = document.querySelector(".box-3");
+var clear = document.querySelector("button");
+
+
 
 
 document.addEventListener("keydown", function(e){
     var key = e.key;
-    if(key == 1){
+    if(key == "1"){
         box1.innerText = "🌲";
 
-    } else if (key == 2){
+    } else if (key == "2"){
         box2.innerText = "🪅";
-    } else if (key == 3){
-        box3.innerHTML = "💃";
+    } else if (key == "3"){
+        box3.innerText = "💃";
 
     }
 
+});
+
+
+clear.addEventListener("click", function(){
+    box1.innerText = "1";
+    box2.innerText = "2";
+    box3.innerText = "3";
 });
